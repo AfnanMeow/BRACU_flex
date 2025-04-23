e9a6f961d15763cb2dfb93e7378d115e7ac67c8d
shihabsararlabib <100600464+shihabsararlabib@users.noreply.github.com>
2025-04-23 22:37:57 +0600

    Delete module2 directory

--- module2/forms.py
+++
@@ -1,8 +0,0 @@
-# forms.py
-from django import forms
-from .models import UploadedVideo
-
-class UploadedVideoForm(forms.ModelForm):
-    class Meta:
-        model = UploadedVideo
-        fields = ['title', 'video_file', 'is_public']
--- module2/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import upload_video, my_uploads, public_videos
-
-urlpatterns = [
-    path('upload/', upload_video, name='upload_video'),
-    path('my-uploads/', my_uploads, name='my_uploads'),
-    path('public/', public_videos, name='public_videos'),
-]
